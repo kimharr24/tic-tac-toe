b66a18df0b6dbc339eb3c3581d70414353023fc7
Harrison Kim <kim.harr@northeastern.edu>
2022-11-29 21:00:16 -0500

    basic tic tac toe completed

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,24 +1,57 @@
 import React, { useState } from 'react';
 import Square from './Square';
 
-const Board = (props) => {
+const initialBoard = Array(9).fill(null);
+
+const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+}
+
+const Board = () => {
+    const [squares, setSquares] = useState(initialBoard);
+    const [xIsNext, setXIsNext] = useState(true);
+
     const renderSquare = (i) => {
-        return <Square value={squares[i]} handleClick={() => handleClick(i)} />
+        return <Square value={squares[i]} onClick={() => handleClick(i)} />
+    }
+
+    const winner = calculateWinner(squares);
+
+    let status;
+    if (winner) {
+        status = `${winner} is the winner!`
+    }
+    else {
+        status = `${xIsNext ? 'X' : 'O'} is next!`
     }
 
     const handleClick = (i) => {
+        // If a player has won or the square is already populated
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         const squaresCopy = squares.slice();
         squaresCopy[i] = xIsNext ? 'X' : 'O';
         setSquares(squaresCopy);
         setXIsNext(!xIsNext);
     }
-
-    const initialBoard = Array(9).fill(null);
-    
-    const [squares, setSquares] = useState(initialBoard);
-    const [xIsNext, setXIsNext] = useState(true);
-
-    const status = `Next Player: ${xIsNext ? 'X' : 'O'}`
 
     return (
         <div>
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const Square = (props) => {
-    console.log(props.handleClick)
+const Square = ({ onClick, value }) => {
     return (
-        <button className="square" onClick={props.handleClick}>{props.value}</button>
+        <button className="square" onClick={onClick}>{value}</button>
     );
 }
 
