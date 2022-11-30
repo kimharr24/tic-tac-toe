bf48c0db148a7517c64aaffbedd6572bebc27b75
Harrison Kim <kim.harr@northeastern.edu>
2022-11-30 00:04:02 -0500

    refactored state with useContext

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { HandleClickContext } from '.';
+import { SquaresContext } from '.';
 
-const Square = ({ onClick, value }) => {
+const Square = ({ index }) => {
+    const onClick = useContext(HandleClickContext);
+    const squares = useContext(SquaresContext);
+
     return (
-        <button className="square" onClick={onClick}>{value}</button>
+        <button className="square" onClick={() => onClick(index)}>{squares[index]}</button>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Board from './Board';
+import calculateWinner from './CalculateWinner';
 
-const Game = (props) => {
+export const HandleClickContext = React.createContext();
+export const SquaresContext = React.createContext();
+
+const Game = () => {
+    const [boardState, setBoardState] = useState({
+        history: [{
+            squares: Array(9).fill(null)
+        }],
+        stepNumber: 0,
+        xIsNext: true,
+    })
+
+    const currentBoard = boardState.history[boardState.stepNumber];
+
+    const handleClick = (i) => {
+        if (calculateWinner(currentBoard.squares) || currentBoard.squares[i]) {
+            return;
+        }
+
+        const squaresCopy = currentBoard.squares.slice();
+        squaresCopy[i] = boardState.xIsNext ? 'X' : 'O';
+        setBoardState({
+            history: boardState.history.concat({
+                squares: squaresCopy
+            }),
+            stepNumber: boardState.history.length,
+            xIsNext: !boardState.xIsNext
+        })
+    }
+
+    let status;
+    if (calculateWinner(currentBoard.squares)) {
+        status = `${calculateWinner(currentBoard.squares)} is the winner!`
+    }
+    else {
+        status = `${boardState.xIsNext ? 'X' : 'O'} is next!`
+    }
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                {status}
+                <HandleClickContext.Provider value={i => handleClick(i)}>
+                    <SquaresContext.Provider value={currentBoard.squares}>
+                        <Board />
+                    </SquaresContext.Provider>
+                </HandleClickContext.Provider>
             </div>
             <div className="game-info">
-                <div></div>
-                <ol></ol>
+                <ol>
+                    
+                </ol>
             </div>
         </div>
     )
